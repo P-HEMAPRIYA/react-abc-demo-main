32e6a5c3a71a35d725daf4d700b27fc2fd44787e
Hemapriya P <hemapriya.pandian@hurix.com>
2023-08-09 07:28:51 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -168,13 +168,3 @@ function Home() {
     </div>
   );
 }
-
-export function ColorBox({ clr }) {
-  const styles = {
-    height: "25px",
-    width: "250px",
-    background: clr,
-    marginTop: "5px",
-  };
-  return <div style={styles}></div>;
-}
